0ec66aa658418f6ae6706a59e79ff29e25930e73
BhaweshVerma <bhaweshverma50@gmail.com>
2021-01-31 19:52:20 +0530

    Update index.js

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const port = process.env.PORT;
 const server = http.createServer((req, res) => {
     res.statusCode = 200;
     res.setHeader('Content-Type', 'text/plain');
-    res.end('Hello World');
+    res.end('GIFairy is up and running!');
 });
 
 server.listen(port, hostname, () => {
